83942aa72b9a7a5b64f7ff79fd1cde4c49a7fb96
tomas <64665732+tomasCalletce@users.noreply.github.com>
2024-04-30 13:01:39 -0500

    feat: add k vector

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import numpy as np
 
-# Create a 2x3 array of random numbers
-array = np.random.rand(2, 3)
+class Groover:
+    def __init__(self, numberOfElementsInputDomain):
+        import numpy as np
 
-print("Original Array:")
-print(array)
+        self.numberOfElementsInputDomain = numberOfElementsInputDomain
+        self.numberOfSates = 2**numberOfElementsInputDomain
+        self.register = self.state0()
 
-# Perform element-wise multiplication
-multiplied_array = array * 10
+    def run(self):
+        self.register = self.makeKetH()
+        print(self.register)
 
-print("Array After Multiplication by 10:")
-print(multiplied_array)
 
-# Calculate the mean of the array
-mean_value = np.mean(array)
-print("Mean of the Array:")
-print(mean_value)
+    def state0(self):
+        allzeros = np.zeros(self.numberOfSates)
+        allzeros[0] = 1
+        return allzeros
+    
+    def makeKetH(self):
+        return np.dot(self.hadamardN(), self.register)
+
+
+    def hadamardN(self):
+        hadamardForNSates = self.hadamard()
+
+        for i in range(self.numberOfElementsInputDomain - 1):
+            hadamardForNSates = np.kron(hadamardForNSates, self.hadamard())
+
+        return hadamardForNSates
+    
+    def hadamard(self):
+        matrix = np.ones((2, 2))
+        matrix[1][1] = -1
+
+        return 1/np.sqrt(2) * matrix
+
+
+groover_instance = Groover(numberOfElementsInputDomain=2)
+groover_instance.run()
